f3b045dd17adfe6b9545a788089c694f506190bf
Vladyslav Hurzhyi <soulline0@gmail.com>
2022-10-24 00:00:24 +0200

    weather today done

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import el from 'date-fns/esm/locale/el/index.js';
+import { format } from 'date-fns';
 import { WeatherAPI } from './js/API';
 import { setupDate } from './js/date';
 import { refs } from './js/refs';
@@ -25,12 +26,15 @@ const handleSubmit = async event => {
 refs.searchFormRef.addEventListener('submit', handleSubmit);
 
 async function weatherNow() {
-  refs.searchSectionRef.classList.add('searchOnTop');
-  isHiddenOff();
   try {
     const data = await weatherApi.getWeatherNow();
+    await weatherFiveDay();
+
     refs.searchFormRef.reset();
-    console.log(data);
+
+    isHiddenOff();
+    refs.searchSectionRef.classList.add('searchOnTop');
+    deleteBlur();
 
     const { name, main, weather, wind } = data;
 
@@ -39,12 +43,34 @@ async function weatherNow() {
     refs.humidityValueNow.textContent = `${Math.round(main.humidity)}`;
     refs.pressureValueNow.textContent = `${Math.round(main.pressure)}`;
     refs.weathernowSkyRef.textContent = weather[0].main;
-    refs.windValueNowRef.textContent = `${Math.round(wind.speed)}`;
+    refs.windValueNowRef.textContent = wind.speed;
+  } catch (error) {
+    // console.log(error);
+    refs.notifyRef.classList.remove('is-hidden');
+    setTimeout(() => {
+      refs.notifyRef.classList.add('is-hidden');
+    }, 2000);
+    return;
+  } finally {
+    refs.spinnerRef.classList.add('is-hidden');
+  }
+}
+
+async function weatherFiveDay() {
+  try {
+    const { list } = await weatherApi.getWeatherFiveDay();
+    renderTimeOnHour(list);
+
+    // _____
+    renderTempOnHour(list);
+
+    ///
+
+    renderWeekData(list);
+
+    console.log(list);
   } catch (error) {
     console.log(error);
-  } finally {
-    deleteBlur();
-    refs.spinnerRef.classList.add('is-hidden');
   }
 }
 
@@ -59,3 +85,62 @@ export function isHiddenOff() {
   refs.wperDayRef.classList.remove('is-hidden');
   refs.weatherWeekRef.classList.remove('is-hidden');
 }
+
+export function renderTimeOnHour(list) {
+  const timeOnPage = [];
+  refs.byTimeHours.forEach(time => {
+    timeOnPage.push(time.textContent);
+  });
+
+  for (let index = 0; index < timeOnPage.length; index += 1) {
+    let newTime = [];
+    list.forEach(timeData => newTime.push(timeData.dt_txt.slice(11, 16)));
+
+    timeOnPage[index] = newTime[index];
+  }
+
+  refs.byTimeHours.forEach((time, index) => {
+    time.textContent = timeOnPage[index];
+  });
+}
+
+export function renderTempOnHour(list) {
+  const tempOnPage = [];
+  refs.byTimeTemp.forEach(item => {
+    tempOnPage.push(item.textContent);
+  });
+
+  for (let index = 0; index < tempOnPage.length; index += 1) {
+    let newTemp = [];
+
+    list.forEach(tempData => newTemp.push(Math.round(tempData.main.temp)));
+
+    tempOnPage[index] = newTemp[index];
+  }
+
+  refs.byTimeTemp.forEach((item, index) => {
+    item.textContent = tempOnPage[index] + '℃';
+  });
+}
+
+// _____
+
+// export function renderWeekData(list) {
+//   const dataNow = list[0].dt;
+
+//   const dataTomorow = dataNow + 86400 * 1000;
+
+//   console.log(dataTomorow);
+
+//   // const nightTime = '00:00:00';
+//   // const morningTime = '09:00:00';
+//   // const dayTime = '12:00:00';
+//   // const eveningTime = '18:00:00';
+
+//   const dateTomorrowNight = format(
+//     Date(dataTomorow),
+//     'yyyy' + '-' + 'M' + '-' + 'd' + ' ' + '00:00:00'
+//   );
+
+//   console.log(dateTomorrowNight);
+// }
--- src/js/API.js
+++ src/js/API.js
@@ -1,13 +1,23 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://api.openweathermap.org/data/2.5/weather';
+axios.defaults.baseURL = 'https://api.openweathermap.org/data/2.5/';
 
 export class WeatherAPI {
   #api = '1ebe2741fc2dad72428940b7d498d033';
   #city = '';
 
   async getWeatherNow() {
-    const urlAXSIOS = `?q=${this.#city}&appid=${this.#api}&units=metric`;
+    const urlAXSIOS = `weather?q=${this.#city}&appid=${this.#api}&units=metric`;
+
+    const { data } = await axios.get(urlAXSIOS);
+
+    return data;
+  }
+
+  async getWeatherFiveDay() {
+    const urlAXSIOS = `forecast?q=${this.#city}&appid=${
+      this.#api
+    }&units=metric`;
 
     const { data } = await axios.get(urlAXSIOS);
 
